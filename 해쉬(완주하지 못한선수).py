957ba7b3ab556a10eecae483829a6160a91e89fc
as2396 <57715611+as2396@users.noreply.github.com>
2020-06-20 23:50:53 +0900

    hash01

--- 해쉬(완주하지 못한선수).py
+++ 해쉬(완주하지 못한선수).py
@@ -1,4 +1,16 @@
-name = input()
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for name1,name2 in zip(participant,completion):
+        if name1 != name2 :
+            return name1
+    return participant[-1]
 
-print(name)
-print(name)
+
+
+
+participant = ['leo', 'kiki', 'eden']
+completion = ['eden', 'kiki']
+result = solution(participant,completion)
+
+print(result)
