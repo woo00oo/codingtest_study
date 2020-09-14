4eaa0f09da38777c480c041d0482b1fcd0549bb7
as2396 <57715611+as2396@users.noreply.github.com>
2020-09-14 23:08:49 +0900

    sorting02

---
+++ venv/정렬(가장큰수).py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+    return str(int(''.join(numbers)))
+
+
+numbers = [31,32,3, 34, 5, 9]
+
+print(solution(numbers))
+
