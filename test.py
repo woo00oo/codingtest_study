def7dc272505595759885aae7c57dfa186e29de3
woo00oo <96woo94@naver.com>
2020-09-19 10:32:11 +0900

    연결 리스트 구현

--- test.py
+++
@@ -1,6 +0,0 @@
-
-
-test = {"한국","홍콩","중국"}
-test.update({"베트남",""})
-
-print(test)
---
+++ 연결리스트 구현.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,data,next = None):
+        self.data = data
+        self.next = next
+
+class NodeMgn:
+    def __init__(self,data):
+        self.head = Node(data)
+
+    def add(self,data):
+        if self.head == '':
+            self.head = Node(data)
+        else:
+            node = self.head
+            while node.next:
+                node = node.next
+            node.next = Node(data)
+
+    def desc(self):
+        node = self.head
+        while node :
+            print(node.data)
+            node = node.next
+
+
+linkedlist1 = NodeMgn(0)
+#linkedlist1.desc()
+
+for i in range(1,10):
+    linkedlist1.add(i)
+
+linkedlist1.desc()
