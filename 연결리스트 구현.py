c1f25e0a13f4b4c9d55cf0b3518d7bddd080a97f
woo00oo <96woo94@naver.com>
2020-09-19 10:43:53 +0900

    연결 리스트 삭제 기능 구현

--- 연결리스트 구현.py
+++ 연결리스트 구현.py
@@ -20,7 +20,27 @@ class NodeMgn:
         node = self.head
         while node :
             print(node.data)
-            node = node.next
+            node = node.next현
+
+    def delete(self,data):
+        if self.head == '':
+            print("데이터가 존재하지 않습니다.")
+            return
+        if self.head.data == data: #첫번째 노드 삭제일 경
+            temp = self.head
+            self.head = self.head.next
+            del temp
+        else: #중간, 마지막 노드 삭제일 경
+            node = self.head
+            while node.next:
+                if node.next.data == data:
+                    temp = node.next
+                    node.next = node.next.next
+                    del temp
+                    return
+                else:
+                    node = node.next
+
 
 
 linkedlist1 = NodeMgn(0)
