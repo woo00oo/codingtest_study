b348605f30b3b05862bee7f6e76245a82b5aa78d
woo00oo <96woo94@naver.com>
2020-09-20 10:52:48 +0900

    해쉬 테이블 구현

---
+++ 해쉬테이블.py
@@ -0,0 +1,21 @@
+hash_table = list([i for i in range(10)])
+
+#해싱 함수
+def hash_func(key):
+    return key % 5
+
+#해쉬 테이블에 값을 저장하는 함수
+def storage_data(data,value):
+    key = ord(data[0])
+    hash_address = hash_func(key)
+    hash_table[hash_address] = value
+
+#해쉬 테이블에서 값을 가져오는 함수
+def get_data(data):
+    key = ord(data[0])
+    hash_address = hash_func(key)
+    return hash_table[hash_address]
+
+
+
+
