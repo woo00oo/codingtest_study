f0fd0111e2a7a54dc332a38fceb57952394fa0c7
woo00oo <96woo94@naver.com>
2020-09-21 19:13:19 +0900

    BTS 삭제기능 구현1

--- 트리(Tree).py
+++ 트리(Tree).py
@@ -35,3 +35,44 @@ class NodeMgmt:
                 self.current_node = self.current_node.right
 
         return False
+
+    def delete(self,value):
+        searched = False
+        self.current_node = self.head
+        self.parent = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                searched = True
+                break
+            elif value < self.current_node.value:
+                self.parent = self.current_node
+                self.current_node = self.current_node.left
+            else:
+                self.parent = self.current_node
+                self.current_node = self.current_node.right
+        if searched == False:
+            return False
+
+        #case1 : 삭제할 노드가 단말 노드일 경우
+        if self.current_node.left == None and self.current_node.right == None:
+            if value <self.parent.value:
+                self.parent.left = None
+            else:
+                self.parent.right = None
+            del self.current_node
+
+        #case2 : 삭제할 Node가 자식노드를 한 개 가지고 있을 경우
+
+        #삭제할 노드의 자식노드가 왼쪽에 있을 경우
+        if self.current_node.left != None and self.current_node.right == None:
+            if value < self.parent.value:
+                self.parent.left = self.current_node.left
+            else:
+                self.parent.right = self.current_node.left
+
+        #삭제할 노드의 자식노드가 오른쪽에 있을 경우
+        elif self.current_node.left == None and self.current_node.right != None:
+            if value < self.parent.value:
+                self.parent.left = self.current_node.right
+            else:
+                self.parent.right = self.current_node.right
