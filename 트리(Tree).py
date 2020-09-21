2d7f497ef62e1cf16814bbb8196f0713829e731e
woo00oo <96woo94@naver.com>
2020-09-21 18:07:35 +0900

    BTS 탐색

--- 트리(Tree).py
+++ 트리(Tree).py
@@ -8,7 +8,7 @@ class NodeMgmt:
     def __init__(self,head):
         self.head = head
 
-    def inser(self,value):
+    def insert(self,value):
         self.current_node = self.head
         while True:
             if value < self.current_node.value:
@@ -24,3 +24,14 @@ class NodeMgmt:
                     self.current_node.right = Node(value)
                     break
 
+    def search(self,value):
+        self.current_node = self.head
+        while self.current_node:
+            if self.current_node.value == value:
+                return True
+            elif value < self.current_node.value:
+                self.current_node = self.current_node.left
+            else:
+                self.current_node = self.current_node.right
+
+        return False
