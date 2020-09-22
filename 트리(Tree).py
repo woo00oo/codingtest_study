09ce5fb6864ac847fc1947f6995ca346ccfee783
woo00oo <96woo94@naver.com>
2020-09-23 00:49:17 +0900

    BTS 기능 구현 테스트

--- 트리(Tree).py
+++ 트리(Tree).py
@@ -107,3 +107,33 @@ class NodeMgmt:
                 self.parent.right = self.change_node
                 self.change_node.left = self.current_node.left
                 self.change_node.right = self.current_node.right
+
+
+
+#0-999 숫자 중에서 임의로 100개를 추출해서, 이진 탐색 트리에 입력, 검색, 삭제
+import random
+
+bst_nums = set()
+while len(bst_nums) != 100:
+    bst_nums.add(random.randint(0,999))
+
+#선택된 100개의 숫자를 이진 탐색 트리에 입력, 임의로 루트노드는 500으로 생성
+head = Node(500)
+binary_tree = NodeMgmt(head)
+for num in bst_nums:
+    binary_tree.insert(num)
+
+#입력한 100개의 숫자 검색(검색 기능확인)
+for num in bst_nums:
+    if binary_tree.search(num) == False:
+        print('search failed',num)
+
+#입력한 100개의 숫자 중 10개의 숫자를 랜덤 선택
+delete_nums = set()
+bst_nums = list(bst_nums)
+while len(delete_nums) != 10:
+    delete_nums.add(bst_nums[random.randint(0,99)])
+#선택한 10개의 숫자를 삭제(삭제기능확인)
+for del_num in delete_nums:
+    if binary_tree.delete(del_num) == False:
+        print('delet failed',num)
