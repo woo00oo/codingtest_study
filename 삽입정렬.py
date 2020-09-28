43a8661e5ccff2ef0a8a3ffb72feb899eecd90e7
woo00oo <96woo94@naver.com>
2020-09-28 14:21:32 +0900

    삽입 정렬 구현

---
+++ 삽입정렬.py
@@ -0,0 +1,14 @@
+def insertionSort(data):
+    for index in range(len(data)-1 ):
+        for index2 in range(index+1,0,-1):
+            if data[index2] < data[index2-1]:
+                data[index2],data[index2-1] = data[index2-1],data[index2]
+            else:
+                break
+    return data
+
+
+import random
+
+data_list = random.sample(range(100),50)
+print(insertionSort(data_list))
