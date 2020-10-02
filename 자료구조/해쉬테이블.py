ce361619e76c4ba681af6fc68c493e88fa998675
woo00oo <96woo94@naver.com>
2020-10-02 22:26:53 +0900

    해쉬테이블 주석추가

--- 자료구조/해쉬테이블.py
+++ 자료구조/해쉬테이블.py
@@ -6,7 +6,7 @@ def hash_func(key):
 
 #해쉬 테이블에 값을 저장하는 함수
 def storage_data(data,value):
-    key = ord(data[0])
+    key = ord(data[0]) #문자의 아스키 코드를 돌려주는 함수
     hash_address = hash_func(key)
     hash_table[hash_address] = value
 
