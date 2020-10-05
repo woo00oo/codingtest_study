010df94aacd0e7b0e39f74fc8583561a97c05cc7
woo00oo <96woo94@naver.com>
2020-10-05 10:56:18 +0900

    그리디 알고리즘(부분배낭문제)

---
+++ 그리디 알고리즘/부분배낭문제.py
@@ -0,0 +1,18 @@
+data_list = [(10,10),(20,10),(30,5),(15,12),(25,8)]
+
+def get_max_value(data_list, capacity):
+    data_list.sort(key=lambda x: x[1] / x[0], reverse=True)
+    total_value = 0
+    details = list()
+
+    for data in data_list:
+        if capacity-data[0] >= 0:
+            capacity -= data[0]
+            total_value += data[1]
+            details.append(data[0],data[1],1)
+        else:
+            fraction = capacity / data[0]
+            total_value += data[1] * fraction
+            details.append(data[0], data[1], fraction)
+            break
+    return total_value,details
