db1473d77825da02e3cad4265700d294c8182e5c
woo00oo <96woo94@naver.com>
2020-10-14 16:43:53 +0900

    개선된 프림 알고리즘(노드 중심)

---
+++ 최소신장알고리즘(MST)/개선된 프림 알고리즘.py
@@ -0,0 +1,30 @@
+# 간선이 아닌 노드를 중심으로 우선순위 큐를 적용
+ygraph = {
+    'A': {'B': 7, 'D': 5},
+    'B': {'A': 7, 'D': 9, 'C': 8, 'E': 7},
+    'C': {'B': 8, 'E': 5},
+    'D': {'A': 5, 'B': 9, 'E': 7, 'F': 6},
+    'E': {'B': 7, 'C': 5, 'D': 7, 'F': 8, 'G': 9},
+    'F': {'D': 6, 'E': 8, 'G': 11},
+    'G': {'E': 9, 'F': 11}
+}
+
+
+from heapdict import heapdict
+
+def prim(graph, start):
+    mst, keys, pi, total_weight = list(), heapdict(), dict(), 0
+    for node in graph.keys():
+        keys[node] = float('inf')
+        pi[node] = None
+    keys[start], pi[start] = 0, start
+
+    while keys:
+        current_node, current_key = keys.popitem()
+        mst.append([pi[current_node], current_node, current_key])
+        total_weight += current_key
+        for adjacent, weight in mygraph[current_node].items():
+            if adjacent in keys and weight < keys[adjacent]:
+                keys[adjacent] = weight
+                pi[adjacent] = current_node
+    return mst, total_weight
