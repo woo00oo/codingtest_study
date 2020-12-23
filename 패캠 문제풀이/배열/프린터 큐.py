0cdfb307a1d13b3205407674b2bed7120c40a4dd
woo00oo <96woo94@naver.com>
2020-12-23 21:29:12 +0900

    프린터 큐

---
+++ 패캠 문제풀이/배열/프린터 큐.py
@@ -0,0 +1,24 @@
+testNumber = int(input())
+
+
+for index in range(0,testNumber):
+
+    length, document = list(map(int,input().split(' ')))
+    queue = list(map(int,input().split(' ')))
+    queue = [(i,idx) for idx, i in enumerate(queue)]
+
+    count = 0
+    while True:
+        if queue[0][0] == max(queue, key=lambda x:x[0])[0]:
+            count += 1
+            if queue[0][1] == document:
+                print(count)
+                break
+            else:
+                queue.pop(0)
+        else:
+            queue.append(queue.pop(0))
+
+
+
+
