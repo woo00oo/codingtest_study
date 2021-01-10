a9caa056b94d47f22ffbd43dedc5f5fe69346350
woo00oo <96woo94@naver.com>
2021-01-10 22:53:19 +0900

    밑 줄- 그리디 알고리즘

--- 백준/그리디알고리즘/밑줄.py
+++ 백준/그리디알고리즘/밑줄.py
@@ -1,43 +1,63 @@
-#문제 해결 :
-# 넣어야 할 밑줄의 개수는 s=m-(단어 길이의 총합)
+# 문제해결 :
+# 예를 들어 아래와 같이 단어가 5개 있고, m=30 이라고 하면,
 #
-# 1. 일단 밑줄을 단어 사이에 균등하게 분배 (단어가 n개면 구간은 n-1개)
-# 즉, 각 단어 사이에 s/(n-1) 개의 밑줄을 쓴다.
-# 2. 이제 남은 밑줄 개수는 s%(n-1)개.
-# 남은 밑줄을 앞쪽에서부터 소문자로 시작하는 단어의 앞쪽에 1개씩 최대한 배치. (맨 앞 단어는 제외)
-# 3. 이렇게 하고 나서도 남은 밑줄이 있으면 뒤쪽에서부터 대문자로 시작하는 단어의 앞에 1개씩 최대한 배치. (맨 앞 단어는 제외)
+# abc
+# Abc
+# aBC
+# ABC
+# aBc
+#
+# 그러면 밑줄은 30-15=15개 추가
+# 단어가 5개니, 밑줄을 넣을 수 있는 위치(구간)는 4개
+#
+# 1. 최대한 균등하게 밑줄을 넣어야 하므로, 먼저 각 위치에 밑줄을 15/4=3 개씩 써줌
+#
+# abc___Abc___aBC___ABC___aBc
+#
+# 2. 밑줄을 15-4*3= 3개 더 넣어야 함. 밑줄을 넣어서 사전순으로 더 앞이 되는 경우는 소문자 시작 단어의 바로 앞쪽에 넣는 것이고, 이 경우 되도록 앞에 위치한 소문자 시작 단어에 적용하는 것이 좋음
+#
+# abc 1 Abc 2 aBC 3 ABC 4 aBc
+#
+# 소문자로 시작하는 단어 앞인 곳은 2,  4이다. 남은 밑줄은 3개니까 이 두 위치에 밑줄 1개씩 넣어준다
+#
+# abc___Abc____aBC___ABC____aBc
+#
+# 3. 그래도 남은 밑줄은 어쩔 수 없이 대문자 시작 단어 앞에 넣어야 하는데, 밑줄을 넣으면 사전 순에서 뒤로 밀리게 된다 .
+# 그래서 최대한 뒤로 밀리지 않도록, 뒤쪽부터 대문자 시작 단어 앞에 밑줄을 1개씩 넣어준다. 그러한 위치로는 1, 3 이 있는데 남은 밑줄은 1개이므로 3에만 밑줄을 넣어주면 된다
+#
+# abc___Abc____aBC____ABC____aBc
 
-
-# 제시된 테스트 케이스는 풀리지만 모든 테스트를 성공하는데는 실패...
-
-n, m = list(map(int,input().split()))
+N, M = list(map(int,input().split()))
 voca_list = list()
-a = list()
 
-for i in range(n):
-    voca = input()
-    m -= len(voca)
-    voca_list.append(voca)
-    if voca[0] > '_':
-        a.append(voca)
-    if i != n-1:
-        voca_list.append('_')
-        m -= 1
+for _ in range(N):
+    voca_list.append(input())
 
+S = M - len(''.join(voca_list))
+under_list = [S//(N-1)] * (N-1)
 
-for value in a:
-    if m == 0:
-         break
-    index = voca_list.index(value)
-    if index != 0:
-        voca_list.insert(voca_list.index(value), '_')
-        m -= 1
+remainder = S % (N-1)
 
-index = len(voca_list) - 1
-while m:
-    if voca_list[index] != '_':
-        voca_list.insert(index, '_')
-        m -= 1
-    index -= 1
+if remainder != 0:
+    for i in range(1, N):
+        if remainder == 0 :
+            break
+        if voca_list[i][0] > '_':
+            under_list[i-1] += 1
+            remainder -= 1
 
-print(''.join(voca_list))
+if remainder != 0:
+    for i in range(N-1, 0, -1):
+        if remainder == 0:
+            break
+        if voca_list[i][0] < '_':
+            under_list[i-1] += 1
+            remainder -= 1
+
+answer = list()
+answer.append(voca_list[0])
+for i in range(1,N):
+    answer.append('_' * under_list[i-1])
+    answer.append(voca_list[i])
+
+print(''.join(answer))
