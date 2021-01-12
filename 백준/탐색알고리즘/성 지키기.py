083fd6f020dad7bbfaede3e18756c6930ab4bd87
woo00oo <96woo94@naver.com>
2021-01-13 00:25:13 +0900

    성 지키기 - 탐색 알고리즘

---
+++ 백준/탐색알고리즘/성 지키기.py
@@ -0,0 +1,29 @@
+# 문제해결 :
+#   행기준, 열 기준으로 필요한 경비원의 수를 각각 계산하여 더 큰 수를 출력.
+
+n, m = map(int,input().split())
+array = []
+
+for _ in range(n):
+    array.append(input())
+
+row = [0] * n
+column = [0] * m
+
+for i in range(n):
+    for j in range(m):
+        if array[i][j] == 'X':
+            row[i] = 1
+            column[j] = 1
+
+row_count = 0
+for i in range(n):
+    if row[i] == 0:
+        row_count += 1
+
+column_count = 0
+for j in range(m):
+    if column[j] == 0:
+        column_count += 1
+
+print(max(row_count,column_count))
