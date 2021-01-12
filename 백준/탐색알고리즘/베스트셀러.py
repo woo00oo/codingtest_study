30121e0cb1e12d64b11caf9b2161ba9f6040075a
woo00oo <96woo94@naver.com>
2021-01-12 17:24:31 +0900

    베스트셀러 - 탐색 알고리즘

---
+++ 백준/탐색알고리즘/베스트셀러.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+books = dict()
+
+for _ in range(N):
+    book = input()
+    if book not in books:
+        books[book] = 1
+    else:
+        books[book] += 1
+
+target = max(books.values())
+array = []
+
+for book, number in books.items():
+    if number == target:
+        array.append(book)
+
+print(sorted(array)[0])
