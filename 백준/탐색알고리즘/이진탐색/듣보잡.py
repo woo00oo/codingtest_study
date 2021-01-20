4bac4b91c62f17f01fba3c4d441468c9eb33b9a4
woo00oo <96woo94@naver.com>
2021-01-20 12:01:00 +0900

    듣보잡 - 이진 탐색

---
+++ 백준/탐색알고리즘/이진탐색/듣보잡.py
@@ -0,0 +1,36 @@
+#문제 해결 : 이진 탐색으로 풀어보려고 하였지만 시간복잡도가 약 NlogM *20 약 1억이 넘는 수라 시간초과가 뜸.
+
+
+N, M = list(map(int,input().split()))
+
+N_list = [input() for _ in range(N)]
+M_list = [input() for _ in range(M)]
+N_list.sort()
+M_list.sort()
+
+for n in N_list:
+    start = 0
+    end = M-1
+
+    while start <= end:
+        mid = start + end // 2
+
+        if M_list[mid] == n:
+            print(M_list[mid])
+            break
+        elif M_list[mid] > n:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+#   교집합으로 간단하게 해결.
+
+N, M = list(map(int,input().split()))
+
+N_list = {input() for _ in range(N)}
+M_list = {input() for _ in range(M)}
+
+answer = sorted(list(N_list&M_list))
+print(len(answer))
+for i in answer:
+    print(i)
