2e7d74557932281abb3408c925ac409abb8186d3
woo00oo <96woo94@naver.com>
2021-02-14 21:30:59 +0900

    퇴사(14501) - dp

--- 백준/DP/퇴사(14501).py
+++ 백준/DP/퇴사(14501).py
@@ -12,5 +12,5 @@ for i in range(n - 1, -1, -1):
     if t[i] + i > n:
         dp[i] = dp[i + 1]
     else:
-        dp[i] = max(dp[i + 1], p[i] + dp[i + t[i]])
+        dp[i] = max(dp[i + 1], p[i] + dp[i + t[i]]) # max(현재 일을 하지 않고 넘어갈 때의 이윤(i+1) , 현재 일의 이윤 + 현재 일이 필요한 day뒤의 이윤)
 print(dp[0])
