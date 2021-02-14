d237773b1b16899c489a705a7287a3da01e9a4ce
woo00oo <96woo94@naver.com>
2021-02-14 21:24:53 +0900

    퇴사(14501) - dp

--- 백준/DP/퇴사(14501).py
+++ 백준/DP/퇴사(14501).py
@@ -1,13 +1,13 @@
+# 맨 마지막일 부터 거꾸로 계산.
 n = int(input())
 t = []
 p = []
-dp = []
+dp = [0] * (n+1)
 for i in range(n):
     a, b = map(int, input().split())
     t.append(a)
     p.append(b)
-    dp.append(b)
-dp.append(0)
+
 for i in range(n - 1, -1, -1):
     if t[i] + i > n:
         dp[i] = dp[i + 1]
