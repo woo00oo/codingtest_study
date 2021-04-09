c7efeb695e64152ce4a7a463c7a2fb328e6cbfee
woo00oo <96woo94@naver.com>
2021-04-09 12:56:59 +0900

    최대공약수와 최소공배수

---
+++ 프로그래머스/최대공약수와 최소공배수.py
@@ -0,0 +1,15 @@
+# 유클리드 호제법을 이용한 최대 공약수, 최소 공배수 구하기
+# 최소 공배수 = a * b / 최대 공약수
+
+def gcd(a, b):
+    if a < b:
+        (a, b) = (b, a)
+    while b != 0:
+        (a, b) = (b, a % b)
+    return a
+
+
+def solution(n, m):
+    return [gcd(n, m), n * m / gcd(n, m)]
+
+
