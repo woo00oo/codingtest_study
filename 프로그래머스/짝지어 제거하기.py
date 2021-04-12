de067cd15dde4aac91a18169fb23f5ab50669769
woo00oo <96woo94@naver.com>
2021-04-12 19:38:44 +0900

    짝지어 제거하기

---
+++ 프로그래머스/짝지어 제거하기.py
@@ -0,0 +1,15 @@
+# 스택 자료구조 활용
+
+def solution(s):
+    stack = []
+    for i in s:
+        if len(stack) == 0:
+            stack.append(i)
+        elif stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
