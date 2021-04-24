08ec2ec1d888b421bfa70c3d36330e71706bc1e2
woo00oo <96woo94@naver.com>
2021-04-24 12:10:20 +0900

    단어 변환(문제 해설)

--- 프로그래머스/단어 변환.py
+++ 프로그래머스/단어 변환.py
@@ -1,3 +1,5 @@
+# 한 글자만 다른 단어 = 연결되어있는 노드
+
 answer = 0
 
 
@@ -15,6 +17,7 @@ def dfs(begin, target, words, visited):
 
         for w in range(0, len(words)):
             # 조건 1. 한 개의 알파벳만 다른 경우
+            # words에 있는 한개 한개의 단어가 이전 비교값과 스펠링이 1개만 다르다면 방문여부를 true(1)로 변경하고, stacks에 추가
             if len([i for i in range(0, len(words[w])) if words[w][i] != stack[i]]) == 1:
 
                 if visited[w] != 0:
@@ -36,6 +39,7 @@ def solution(begin, target, words):
     if target not in words:
         return 0
 
+    #방문 여부를 words의 갯수만큼 0으로 초기화
     visited = [0 for i in words]
 
 
@@ -43,3 +47,4 @@ def solution(begin, target, words):
 
     return answer
 
+print(solution('hit','dog',["hot", "dot", "dog", "lot", "log", "cog"]))
