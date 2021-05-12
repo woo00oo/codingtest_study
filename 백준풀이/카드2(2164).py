fa86d9df4b10f434753c4048e854957f3aa1c33e
woo00oo <96woo94@naver.com>
2021-05-12 12:22:33 +0900

    카드2(2164)

---
+++ 백준풀이/카드2(2164).py
@@ -0,0 +1,33 @@
+# 리스트를 활용하면 시간 초과
+
+
+from collections import deque
+
+# N = int(input())
+# stack = [i for i in range(1, N + 1)]
+#
+# while stack:
+#     if len(stack) == 1:
+#         print(*stack)
+#         exit(0)
+#
+#     # 맨위의 카드를 버린다.
+#     stack.pop(0)
+#
+#     # 그 다음 맨 위에 있는 카드를 아래에 있는 카드 밑으로 옮긴다.
+#     card = stack.pop(0)
+#     stack.append(card)
+
+
+N = int(input())
+queue = deque([i for i in range(1, N+1)])
+
+while queue:
+    if len(queue) == 1:
+        print(*queue)
+        exit(0)
+
+    queue.popleft()
+
+    card = queue.popleft()
+    queue.append(card)
