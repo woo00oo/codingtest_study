65d2832ba750646bb4fffc6968303ca8635161fc
woo00oo <96woo94@naver.com>
2021-07-01 23:54:40 +0900

    백준 - 쇠막대기(10799)

---
+++ 백준풀이/쇠막대기(10799).py
@@ -0,0 +1,26 @@
+# 스택을 이용해서 풀 수 있다.
+
+# 1. (이 나오면 스택에 넣는다.
+#
+# 2. ()이 나오면 현재 스택에 있는 ( 수만큼 정답에 더해준다.
+#
+# 3. )이 나오면 스택의 (를 pop하고 정답에 1을 더해준다.
+
+bar_razor = list(input())
+answer = 0
+stack = []
+
+for i in range(len(bar_razor)):
+    if bar_razor[i] == '(':  # 스택 쌓기
+        stack.append('(')
+
+    else:
+        if bar_razor[i - 1] == '(':  # ()라면 (를 pop하고 현재 스택에 들어있는 ( 수만큼 값을 더해준다.
+            stack.pop()
+            answer += len(stack)
+
+        else:
+            stack.pop()
+            answer += 1  # 끄트머리 막대기 부분을 더해준다
+
+print(answer)
