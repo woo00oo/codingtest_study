7bc08cf7021da5e62cba6c9488a35e8701f609a3
woo00oo <96woo94@naver.com>
2021-08-04 13:28:21 +0900

    해커랭크 : Find the Runner-Up Score!

---
+++ 해커랭크/Find the Runner-Up Score!.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    top1 = arr.pop()
+    while True:
+        if top1 != arr[-1]:
+            print(arr[-1])
+            break
+        else:
+            arr.pop()
